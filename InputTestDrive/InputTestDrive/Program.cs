6d7ebc8ee7107bf020ae715c329f8ff363837277
Jeff Mayer <jeff.j.mayer@gmail.com>
2020-07-06 14:13:04 -0500

    Implemented Input code

---
+++ InputTestDrive/InputTestDrive/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace InputTestDrive
+{
+    public class TestDrive
+    {
+        static void Main()
+        {
+            var input = new Input();
+            input.CustomerInput();
+        }
+    }
+
+    public abstract class InputWithHook
+    {
+        public void CustomerInput()
+        {
+            CustomerWants();
+        }
+
+        public void Serve(int answer)
+        {
+            Console.WriteLine("\n" + "Serving " + answer + "\n");
+        }
+
+        public virtual int CustomerWants()
+        {
+            return 0;
+        }
+    }
+
+    public class Input : InputWithHook
+    {
+        public override int CustomerWants()
+        {
+            return GetUserInput();
+        }
+
+        public int GetUserInput()
+        {
+            Console.WriteLine("How many items would you like? ");
+
+            try
+            {
+                int answer = Convert.ToInt32(Console.ReadLine());
+
+                Serve(answer);
+                CustomerInput();
+            }
+            catch
+            {
+                Console.WriteLine("IO error trying to read your answer needs to be a small integer" + "\n");
+                CustomerInput();
+            }
+
+            return 0;
+        }
+    }
+}
